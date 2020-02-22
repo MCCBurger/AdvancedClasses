b3bf5e99d0760114dda5eb4360916d18e17bef8e
Brian <bburger@students.mchenry.edu>
2020-02-22 14:30:17 -0600

    Initial commit

---
+++ src/Employee.kt
@@ -0,0 +1,44 @@
+import java.text.DecimalFormat
+
+class Employee(
+    var name: String,
+    var position: PositionTitle,
+    var salary: Boolean,
+    var payRate: Double,
+    var shift: Int,
+    var startDate: String
+) {
+    //Declare and initialize variables
+    val currency = DecimalFormat("\$###,###,###.00")
+    var payment = 0.0;
+    var weeklyPay = ""
+
+    //Calculate weekly pay based on rate,shift, and hours worked
+    fun calculate(hoursIn: Double) {
+
+        if (shift == 2)
+            payRate = payRate * 1.05
+
+        if (shift == 3)
+            payRate = payRate * 1.10
+
+        if (hoursIn <= 40.00)
+            payment = payRate * hoursIn
+        else {
+            payment = payRate * 40
+        }
+
+        if (!salary)
+            if (hoursIn > 40)
+                payment += (hoursIn - 40) * 1.5 * payRate
+
+        weeklyPay = currency.format(payment)
+
+    }
+
+    //Print all employee information
+    fun display() {
+        println("\n$name\nStart Date: $startDate\n$position Is salary: $salary shift: $shift\nRate of Pay is: ${currency.format(payRate)} per hour\n$name earned $weeklyPay this week\n")
+    }
+
+}
---
+++ src/main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //Declarre and initialize 3 employees
+    val emp1 = Employee("Jane Doe", PositionTitle.ADMINISTRATION, true, 27.00, 1, "10/26/1985")
+    val emp3 = Employee("John Smith", PositionTitle.MAINTENANCE, false, 21.00, 3, "11/5/1955")
+    val emp2 = Employee("Larry Jackson", PositionTitle.PRODUCTION, false, 22.00, 2, "10/21/2015")
+
+    //Add hours and print summary for each employee
+    emp1.calculate(45.0);
+    emp1.display();
+    emp2.calculate(50.0);
+    emp2.display();
+    emp3.calculate(35.0);
+    emp3.display();
+
+}
